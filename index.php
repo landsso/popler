9b29120e9743f5eb5c1d154a1d7b51c9b8b903c8
landsso <cloudfareallsystem@gmail.com>
2026-06-17 14:14:28 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 header('Content-Type: application/json');
 
 echo json_encode([
-    'php_version' => PHP_VERSION,
-    'imagick_extension' => extension_loaded('imagick'),
-    'proc_open' => function_exists('proc_open'),
-    'exec' => function_exists('exec'),
-    'shell_exec' => function_exists('shell_exec'),
-    'pdftotext' => trim(shell_exec('which pdftotext')),
-    'pdfimages' => trim(shell_exec('which pdfimages')),
-    'ghostscript' => trim(shell_exec('which gs')),
-    'imagemagick' => trim(shell_exec('which magick')),
+    'php_version'        => PHP_VERSION,
+    'imagick_extension'  => extension_loaded('imagick'),
+    'imagick_class'      => class_exists('Imagick'),
+    'proc_open'          => function_exists('proc_open'),
+    'exec'               => function_exists('exec'),
+    'shell_exec'         => function_exists('shell_exec'),
+    'pdftotext'          => trim(shell_exec('which pdftotext')),
+    'pdfimages'          => trim(shell_exec('which pdfimages')),
+    'ghostscript'        => trim(shell_exec('which gs')),
+    'imagemagick'        => trim(shell_exec('which magick')),
 ], JSON_PRETTY_PRINT);
